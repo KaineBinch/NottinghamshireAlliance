10f21a66b63a02260eb8df73a928b0bac2e11f96
KaineBinch <kainebinch@live.com>
2024-04-06 02:19:15 +0100

    feat: added GC objects

--- src/constants/golfClubs.js
+++ src/constants/golfClubs.js
@@ -1,6 +1,14 @@
 import coxmoor from "../assets/courses/coxmoor.png"
 import worksop from "../assets/courses/worksop.png"
 import newark from "../assets/courses/newark.png"
+import radcliffe from "../assets/courses/radcliffe.png"
+import bondhay from "../assets/courses/bondhay.png"
+import wollaton from "../assets/courses/wollaton.png"
+import brierly from "../assets/courses/brierly.png"
+import ramsdale from "../assets/courses/ramsdale.png"
+import oakmere from "../assets/courses/oakmere.png"
+import ruddington from "../assets/courses/ruddington.png"
+import bulwell from "../assets/courses/bulwell.png"
 
 export const clubs = [
   {
@@ -32,5 +40,83 @@ export const clubs = [
     year: "2024",
     comp: "Order of Merit",
     courseImage: newark
-  }
+  },
+  {
+    name: "Radcliffe On Trent",
+    address: "Dewberry Ln, Radcliffe on Trent, Nottingham NG12 2JH",
+    dayName: "Thurs",
+    day: "07",
+    month: "September",
+    year: "2023",
+    comp: "Order of Merit",
+    courseImage: radcliffe
+  },
+  {
+    name: "Bondhay Golf Club",
+    address: "Bondhay Ln, Whitwell Common, Worksop S80 3EH",
+    dayName: "Wed",
+    day: "27",
+    month: "September",
+    year: "2023",
+    comp: "Order of Merit",
+    courseImage: bondhay
+  },
+  {
+    name: "Wollaton Park Golf Club",
+    address: "Lime Tree Ave, Nottingham NG8 1BT",
+    dayName: "Tues",
+    day: "10",
+    month: "October",
+    year: "2023",
+    comp: "Order of Merit",
+    courseImage: wollaton
+  },
+  {
+    name: "Brierley Forest Golf Club",
+    address: "160 Main St, Huthwaite, Sutton-in-Ashfield NG17 2LG",
+    dayName: "Wed",
+    day: "18",
+    month: "October",
+    year: "2023",
+    comp: "Team Event Pairs",
+    courseImage: brierly
+  },
+  {
+    name: "Ramsdale Golf Club",
+    address: "Oxton Rd, Calverton, Nottingham NG14 6NU",
+    dayName: "Wed",
+    day: "01",
+    month: "November",
+    year: "2023",
+    comp: "Order of Merit",
+    courseImage: ramsdale
+  },
+  {
+    name: "Oakmere Park Golf Club",
+    address: "Oaks Ln, Oxton, Southwell NG25 0RH",
+    dates: [
+      "15/11/2023",
+      "",
+    ],
+    comp: "Order of Merit",
+    courseImage: oakmere
+  },
+  {
+    name: "Ruddington Golf Club",
+    address: "Wilford Rd, Ruddington, Nottingham NG11 6NB",
+    dates: [
+      "28/11/2023",
+    ],
+    comp: "Order of Merit",
+    courseImage: ruddington
+  },
+  {
+    name: "Bulwell Forest Golf Club",
+    address: "Forest Golf Club, Hucknall Rd, Bulwell, Nottingham NG6 9LQ",
+    dates: [
+      "17/01/2024",
+    ],
+    comp: "Order of Merit",
+    courseImage: bulwell
+  },
 ]
