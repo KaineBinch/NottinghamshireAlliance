e58601bc2f3b1f95101b23598805ae3b7b481b89
Kaine Binch <kainebinch@live.com>
2024-06-11 00:40:06 +0100

    feat: created blank page templates, added navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,11 @@
 import HomePage from "./homePage"
 import NotFound from "./notFound"
+import ContactPage from "./contactPage"
+import CoursesPage from "./coursesPage"
+import FixturesPage from "./fixturesPage"
+import GalleryPage from "./galleryPage"
+import ResultsPage from "./resultsPage"
+import RulesPage from "./rulesPage"
+import StartTimesPage from "./startTimesPage"
 
-export { HomePage, NotFound }
+export { HomePage, NotFound, ContactPage, CoursesPage, FixturesPage, GalleryPage, ResultsPage, RulesPage, StartTimesPage }
