24912b578afe8f07c30ae9dc53f0bbc41109b215
Kaine Binch <kainebinch@live.com>
2024-07-02 17:00:35 +0100

    feat: course card and initial page layout

---
+++ src/components/courseCard.jsx
@@ -0,0 +1,42 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const CourseCard = ({ name, address, contact, link, courseImage }) => {
+  return (
+    <>
+      <div className="flex text-black">
+        <div
+          className="flex w-2/5 h-50 md:h-60"
+          style={{
+            backgroundImage: `url(${courseImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
+        <div className="flex flex-col w-3/5 text-start pl-5 md:pl-10 justify-center">
+          <div className="font-bold text-xl md:text-2xl py-4">{name}</div>
+          <div className="pb-4 md:text-lg">{address}</div>
+          <div className="flex items-center justify-between md:text-lg ">
+            <a href={`tel:${contact}`}>
+              <FontAwesomeIcon
+                icon="fa-solid fa-phone"
+                className="mr-1 md:pr-2"
+              />
+              {contact}
+            </a>
+            <a
+              className="btn border-black rounded-none md:text-lg "
+              href={link}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Visit
+            </a>
+          </div>
+        </div>
+      </div>
+      <hr className="my-4 border-[#A0A0A0] ml-auto w-2/4 md:mr-8 mr-2" />
+    </>
+  );
+};
+
+export default CourseCard;
--- src/pages/coursesPage.jsx
+++ src/pages/coursesPage.jsx
@@ -1,8 +1,29 @@
+import CourseCard from "../components/courseCard";
+import PageHeader from "../components/pageHeader";
+import { clubs } from "../constants/golfClubs";
+
 const CoursesPage = () => {
   return (
     <>
-      <div>
-        <h1>Courses</h1>
+      <PageHeader title="Courses" />
+      <hr className="border-black" />
+      <div className="bg-[#D9D9D9]">
+        <div className="max-w-5xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
+          <div className="">
+            {clubs
+              .sort((a, b) => +(a.name > b.name) * 2 - 0.5)
+              .map((club, i) => (
+                <CourseCard
+                  key={i}
+                  name={club.name}
+                  address={club.address}
+                  contact={club.contact}
+                  link={club.link}
+                  courseImage={club.courseImage}
+                />
+              ))}
+          </div>
+        </div>
       </div>
     </>
   );
