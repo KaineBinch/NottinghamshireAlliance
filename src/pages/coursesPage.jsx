e635d68ba3ec50cf958f543ea330b25c004706a6
Kaine Binch <81770661+KaineBinch@users.noreply.github.com>
2024-07-02 17:20:45 +0100

    Create Courses Page (#1)
    * feat: page header created
    
    * feat: course card and initial page layout
    
    * Update src/pages/coursesPage.jsx
    
    Co-authored-by: Jacson Curtis <justjcurtis@users.noreply.github.com>
    
    * Update src/pages/coursesPage.jsx
    
    Co-authored-by: Jacson Curtis <justjcurtis@users.noreply.github.com>
    
    * fix: fixed broken function from code review suggestion
    
    ---------
    
    Co-authored-by: Jacson Curtis <justjcurtis@users.noreply.github.com>

--- src/pages/coursesPage.jsx
+++ src/pages/coursesPage.jsx
@@ -1,8 +1,31 @@
+import CourseCard from "../components/courseCard";
+import PageHeader from "../components/pageHeader";
+import { clubs } from "../constants/golfClubs";
+
+const azCompare = (a, b) => +(a > b) * 2 - 0.5;
+
 const CoursesPage = () => {
   return (
     <>
-      <div>
-        <h1>Courses</h1>
+      <PageHeader title="Courses" />
+      <hr className="border-black" />
+      <div className="bg-[#D9D9D9]">
+        <div className="max-w-5xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
+          <div className="">
+            {clubs
+              .sort((a, b) => azCompare(a.name, b.name))
+              .map((club, i) => (
+                <CourseCard
+                  key={i}
+                  name={club.name}
+                  address={club.address}
+                  contact={club.contact}
+                  link={club.link}
+                  courseImage={club.courseImage}
+                />
+              ))}
+          </div>
+        </div>
       </div>
     </>
   );
