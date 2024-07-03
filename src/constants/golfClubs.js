a9bafc89efe502fb11a027c4cc925a44b13eb5c7
Kaine Binch <kainebinch@live.com>
2024-07-04 00:45:23 +0100

    feat: added logos

--- src/constants/golfClubs.js
+++ src/constants/golfClubs.js
@@ -9,6 +9,17 @@ import ramsdale from "../assets/courses/ramsdale.png"
 import oakmere from "../assets/courses/oakmere.png"
 import ruddington from "../assets/courses/ruddington.png"
 import bulwell from "../assets/courses/bulwell.png"
+import coxmoorLogo from "../assets/courses/logos/coxmoorLogo.png"
+import bondhayLogo from "../assets/courses/logos/bondhayLogo.png"
+import worksopLogo from "../assets/courses/logos/worksopLogo.png"
+import newarkLogo from "../assets/courses/logos/newarkLogo.png"
+import radcliffeLogo from "../assets/courses/logos/radcliffeLogo.png"
+import wollatonLogo from "../assets/courses/logos/wollatonLogo.png"
+import brierlyLogo from "../assets/courses/logos/brierlyLogo.png"
+import ramsdaleLogo from "../assets/courses/logos/ramsdaleLogo.png"
+import oakmereLogo from "../assets/courses/logos/oakmereLogo.png"
+import ruddingtonLogo from "../assets/courses/logos/ruddingtonLogo.png"
+import bulwellLogo from "../assets/courses/logos/bulwellLogo.png"
 
 export const clubs = [
   {
@@ -22,6 +33,7 @@ export const clubs = [
     courseImage: coxmoor,
     contact: "01623 557359",
     link: "https://www.coxmoorgolfclub.co.uk/",
+    logo: coxmoorLogo,
   },
   {
     name: "Worksop Golf Club",
@@ -34,6 +46,7 @@ export const clubs = [
     courseImage: worksop,
     contact: "01909 477731",
     link: "https://www.worksopgolfclub.com/",
+    logo: worksopLogo,
   },
   {
     name: "Newark Golf Club",
@@ -46,6 +59,7 @@ export const clubs = [
     courseImage: newark,
     contact: "01636 626282",
     link: "https://www.newarkgolfclub.co.uk/",
+    logo: newarkLogo,
   },
   {
     name: "Radcliffe On Trent",
@@ -58,6 +72,7 @@ export const clubs = [
     courseImage: radcliffe,
     contact: "01159 333000",
     link: "https://www.radcliffeontrentgc.co.uk/",
+    logo: radcliffeLogo,
   },
   {
     name: "Bondhay Golf Club",
@@ -70,6 +85,7 @@ export const clubs = [
     courseImage: bondhay,
     contact: "01909 723608",
     link: "https://bondhaygolfclub.com/",
+    logo: bondhayLogo,
   },
   {
     name: "Wollaton Park Golf Club",
@@ -82,6 +98,7 @@ export const clubs = [
     courseImage: wollaton,
     contact: "0115 9787574",
     link: "https://www.wollatonparkgolfclub.com/",
+    logo: wollatonLogo,
   },
   {
     name: "Brierley Forest Golf Club",
@@ -94,6 +111,7 @@ export const clubs = [
     courseImage: brierly,
     contact: "01623 550761",
     link: "https://www.brierleyforestgolfclub.com/",
+    logo: brierlyLogo,
   },
   {
     name: "Ramsdale Golf Club",
@@ -106,6 +124,7 @@ export const clubs = [
     courseImage: ramsdale,
     contact: "0115 9655600",
     link: "https://ramsdaleparkgc.co.uk/",
+    logo: ramsdaleLogo,
   },
   {
     name: "Oakmere Park Golf Club",
@@ -118,6 +137,7 @@ export const clubs = [
     courseImage: oakmere,
     contact: "0115 9653545",
     link: "https://oakmerepark.co.uk/",
+    logo: oakmereLogo,
   },
   {
     name: "Ruddington Golf Club",
@@ -129,6 +149,7 @@ export const clubs = [
     courseImage: ruddington,
     contact: "0115 9214139",
     link: "https://www.ruddingtongrange.com/",
+    logo: ruddingtonLogo,
   },
   {
     name: "Bulwell Forest Golf Club",
@@ -140,5 +161,6 @@ export const clubs = [
     courseImage: bulwell,
     contact: "0115 9763172",
     link: "https://www.bulwellforestgolfclub.co.uk/",
+    logo: bulwellLogo,
   },
 ]
