7b8a39cd0be7ca6127fb8cc321736df430c527a8
Kaine Binch <kainebinch@live.com>
2024-07-04 16:16:35 +0100

    feat: implemented fixtures page

--- src/pages/fixturesPage.jsx
+++ src/pages/fixturesPage.jsx
@@ -1,9 +1,31 @@
 import PageHeader from "../components/pageHeader";
+import { clubs } from "../constants/golfClubs.js";
+import SmallFixtureCard from "../components/smallFixtureCard";
 
 const FixturesPage = () => {
   return (
     <>
       <PageHeader title="Fixtures" />
+      <hr className="border-black" />
+      <div className="flex place-content-center mt-5">
+        <div className="max-w-5xl mx-5 grid grid-cols-1 lg:grid-cols-2 gap-4 mb-[50px]">
+          {clubs
+            .filter((club, index) => index)
+            .map((club, i) => (
+              <SmallFixtureCard
+                key={i}
+                name={club.name}
+                address={club.address}
+                courseImage={club.courseImage}
+                comp={club.comp}
+                dayName={club.dayName}
+                day={club.day}
+                month={club.month}
+                year={club.year}
+              />
+            ))}
+        </div>
+      </div>
     </>
   );
 };
