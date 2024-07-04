a6195adfebe00f45206108b28b1cb9c76d8df609
Kaine Binch <kainebinch@live.com>
2024-07-04 16:16:20 +0100

    feat: added material ui and start times page

---
+++ src/constants/teeTimes.js
@@ -0,0 +1,47 @@
+export const teeTimes = [
+  {
+    time: "08:10",
+    names: [
+      { name: "Tiger Woods", club: "BUL", isSenior: true },
+      { name: "Rory McIlroy", club: "OP", isSenior: false },
+      { name: "Scotty Scheffler", club: "BON", isSenior: false },
+      { name: "Tommy Fleetwood", club: "WP", isSenior: false }
+    ]
+  },
+  {
+    time: "08:20",
+    names: [
+      { name: "Phil Mickleson", club: "BUL", isSenior: true },
+      { name: "Jack Nicklaus", club: "OP", isSenior: true },
+      { name: "Justin Thomas", club: "BON", isSenior: false },
+      { name: "Matt Fitzpatrick", club: "WP", isSenior: false }
+    ]
+  },
+  {
+    time: "08:30",
+    names: [
+      { name: "John Rahm", club: "BUL", isSenior: false },
+      { name: "Victor Hovland", club: "OP", isSenior: false },
+      { name: "Max Homa", club: "BON", isSenior: false },
+      { name: "Vijay Singh", club: "WP", isSenior: true }
+    ]
+  },
+  {
+    time: "08:40",
+    names: [
+      { name: "Sahith Theegala", club: "BUL", isSenior: false },
+      { name: "Jordan Speith", club: "OP", isSenior: false },
+      { name: "Colin Morikawa", club: "BON", isSenior: false },
+      { name: "Ludvig Aberg", club: "WP", isSenior: false }
+    ]
+  },
+  {
+    time: "08:50",
+    names: [
+      { name: "Tony Finau", club: "BUL", isSenior: true },
+      { name: "Ernie Els", club: "OP", isSenior: true },
+      { name: "Steve Ballesteros", club: "BON", isSenior: true },
+      { name: "Rickie Fowler", club: "WP", isSenior: true }
+    ]
+  }
+];
--- src/pages/startTimesPage.jsx
+++ src/pages/startTimesPage.jsx
@@ -1,10 +1,36 @@
 import PageHeader from "../components/pageHeader";
+import TeeTimesTable from "../components/teeTime";
 
 const StartTimesPage = () => {
   return (
     <>
       <PageHeader title="Order of Play" />
+      <hr className="border-black" />
+      <div className="bg-[#d9d9d9]">
+        <div className="max-w-5xl mx-auto py-5 px-4 sm:px-6 lg:px-8 text-start">
+          <p>
+            Please advise any changes and / or details of ‘unnamed’ players by
+            17:00 Monday prior to the event at the latest
+          </p>
+          <p className="py-5">
+            Please collect your scorecard at least 10 minutes before your start
+            time
+          </p>
+          <p>
+            Entry fee payable if you fail to arrive without prior notification.
+          </p>
+        </div>
+        <hr className="border-black mt-5" />
+      </div>
+      <div className="flex justify-center">
+        <div className="max-w-5xl w-full">
+          <div className="m-5">
+            <TeeTimesTable />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
+
 export default StartTimesPage;
