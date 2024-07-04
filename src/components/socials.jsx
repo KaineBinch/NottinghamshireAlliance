34d917a1e2cf8a4ee48e4c44339ad48af8ceed52
Kaine Binch <kainebinch@live.com>
2024-07-04 02:29:49 +0100

    feat: Converted divs to a tags for clickable links.

--- src/components/socials.jsx
+++ src/components/socials.jsx
@@ -4,13 +4,18 @@ const Socials = ({ link, icon }) => {
   return (
     <>
       <div className="flex w-full text-start items-center text-lg ">
-        <div className="flex items-center justify-center">
+        <a
+          className="flex items-center justify-center"
+          href={link}
+          target="_blank"
+          rel="noreferrer"
+        >
           <FontAwesomeIcon
             icon={icon}
             className="mr-5 w-[35px] h-[35px] my-2"
           />
           {link}
-        </div>
+        </a>
       </div>
     </>
   );
