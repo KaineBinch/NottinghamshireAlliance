abefcd43666b83dfa5decefeb02c6fa6d85fd522
Kaine Binch <kainebinch@live.com>
2024-08-24 21:59:08 +0100

    feat: fixture cards updated padding changes

--- src/components/homepage/fixturesSection.jsx
+++ src/components/homepage/fixturesSection.jsx
@@ -21,9 +21,9 @@ const FixturesSection = () => {
         </div>
       </div>
       <div className="flex place-content-center">
-        <div className="max-w-5xl mx-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-[50px]">
+        <div className="max-w-5xl mx-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-4 mb-[50px]">
           {clubs
-            .filter((club, index) => index < 4)
+            .filter((_, i) => i < 4)
             .map((club, i) => (
               <FixtureCard
                 key={i}
@@ -33,7 +33,7 @@ const FixturesSection = () => {
                 comp={club.comp}
                 dayName={club.dayName}
                 day={club.day}
-                month={club.month}
+                monthName={club.monthName}
                 year={club.year}
               />
             ))}
--- src/pages/fixturesPage.jsx
+++ src/pages/fixturesPage.jsx
@@ -1,18 +1,25 @@
 import PageHeader from "../components/pageHeader";
 import { clubs } from "../constants/golfClubs.js";
-import SmallFixtureCard from "../components/smallFixtureCard";
+import FixtureCard from "../components/fixtureCard";
 
 const FixturesPage = () => {
   return (
     <>
       <PageHeader title="Fixtures" />
       <hr className="border-black" />
-      <div className="flex place-content-center mt-5">
-        <div className="max-w-5xl mx-5 grid grid-cols-1 lg:grid-cols-2 gap-4 mb-[50px]">
-          {clubs
-            .filter((club, index) => index)
-            .map((club, i) => (
-              <SmallFixtureCard
+      <div
+        className="-mt-5 w-full"
+        style={{
+          background: "linear-gradient(to bottom, #214A27, #17331B)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="flex place-content-center mt-5">
+          <div className="max-w-5xl mx-5 grid grid-cols-1 md:grid-cols-2 gap-x-4 lg:grid-cols-4 mb-[50px]">
+            {clubs.map((club, i) => (
+              <FixtureCard
                 key={i}
                 name={club.name}
                 address={club.address}
@@ -20,13 +27,15 @@ const FixturesPage = () => {
                 comp={club.comp}
                 dayName={club.dayName}
                 day={club.day}
-                month={club.month}
+                monthName={club.monthName}
                 year={club.year}
               />
             ))}
+          </div>
         </div>
       </div>
     </>
   );
 };
+
 export default FixturesPage;
