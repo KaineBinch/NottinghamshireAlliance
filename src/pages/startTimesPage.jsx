ed0d0a4cb752e1b34602bb32ac7316ba3efd694f
Kaine Binch <kainebinch@live.com>
2024-09-06 01:18:02 +0100

    feat: start times ui change implemented

--- src/pages/startTimesPage.jsx
+++ src/pages/startTimesPage.jsx
@@ -9,15 +9,16 @@ const StartTimesPage = () => {
       <div className="bg-[#d9d9d9]">
         <div className="max-w-5xl mx-auto py-5 px-4 sm:px-6 lg:px-8 text-start">
           <p>
-            Please advise any changes and / or details of ‘unnamed’ players by
-            17:00 Monday prior to the event at the latest
+            Please provide any changes or details regarding ‘unnamed’ players by
+            17:00 on the Monday before the event, at the latest.
           </p>
           <p className="py-5">
-            Please collect your scorecard at least 10 minutes before your start
-            time
+            Please collect your scorecard at least 10 minutes before your
+            scheduled start time.
           </p>
           <p>
-            Entry fee payable if you fail to arrive without prior notification.
+            The entry fee will be payable if you fail to arrive without prior
+            notification.
           </p>
         </div>
         <hr className="border-black my-5" />
