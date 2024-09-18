6ed599165178f5878e248f3278cc733650d1506e
Kaine Binch <kainebinch@live.com>
2024-09-18 19:35:31 +0100

    chore: data change

--- src/constants/golfClubs.js
+++ src/constants/golfClubs.js
@@ -21,19 +21,11 @@ import oakmereLogo from "../assets/courses/logos/oakmereLogo.png"
 import ruddingtonLogo from "../assets/courses/logos/ruddingtonLogo.png"
 import bulwellLogo from "../assets/courses/logos/bulwellLogo.png"
 
-
-
-
 export const clubs = [
   {
     name: "Coxmoor Golf Club",
     address: "Coxmoor House, Coxmoor Road, Sutton-in-Ashfield, NG17 5LF",
     date: "07/03/2024",
-    dayName: "Wed",
-    day: "07",
-    month: "3",
-    monthName: "March",
-    year: "2024",
     comp: "Order of Merit",
     courseImage: coxmoor,
     contact: "01623 557359",
@@ -43,11 +35,7 @@ export const clubs = [
   {
     name: "Worksop Golf Club",
     address: "51 Windmill Lane, Worksop, S80 2SQ",
-    dayName: "Wed",
-    day: "21",
-    month: "3",
-    monthName: "March",
-    year: "2024",
+    date: "21/03/2024",
     comp: "Order of Merit",
     courseImage: worksop,
     contact: "01909 477731",
@@ -57,11 +45,7 @@ export const clubs = [
   {
     name: "Newark Golf Club",
     address: "Sleaford Road, Coddington, Newark, NG24 2QX",
-    dayName: "Wed",
-    day: "04",
-    month: "4",
-    monthName: "April",
-    year: "2024",
+    date: "04/04/2024",
     comp: "Order of Merit",
     courseImage: newark,
     contact: "01636 626282",
@@ -71,11 +55,7 @@ export const clubs = [
   {
     name: "Radcliffe On Trent",
     address: "Dewberry Ln, Radcliffe on Trent, Nottingham NG12 2JH",
-    dayName: "Thurs",
-    day: "07",
-    month: "9",
-    monthName: "September",
-    year: "2024",
+    date: "07/09/2024",
     comp: "Order of Merit",
     courseImage: radcliffe,
     contact: "01159 333000",
@@ -85,11 +65,7 @@ export const clubs = [
   {
     name: "Bondhay Golf Club",
     address: "Bondhay Ln, Whitwell Common, Worksop S80 3EH",
-    dayName: "Wed",
-    day: "27",
-    month: "9",
-    monthName: "September",
-    year: "2024",
+    date: "27/09/2024",
     comp: "Order of Merit",
     courseImage: bondhay,
     contact: "01909 723608",
@@ -99,11 +75,7 @@ export const clubs = [
   {
     name: "Wollaton Park Golf Club",
     address: "Lime Tree Ave, Nottingham NG8 1BT",
-    dayName: "Tues",
-    day: "10",
-    month: "10",
-    monthName: "October",
-    year: "2024",
+    date: "10/10/2024",
     comp: "Order of Merit",
     courseImage: wollaton,
     contact: "0115 9787574",
@@ -113,11 +85,7 @@ export const clubs = [
   {
     name: "Brierley Forest Golf Club",
     address: "160 Main St, Huthwaite, Sutton-in-Ashfield NG17 2LG",
-    dayName: "Wed",
-    day: "18",
-    month: "10",
-    monthName: "October",
-    year: "2024",
+    date: "18/10/2024",
     comp: "Team Event Pairs",
     courseImage: brierly,
     contact: "01623 550761",
@@ -127,11 +95,7 @@ export const clubs = [
   {
     name: "Ramsdale Golf Club",
     address: "Oxton Rd, Calverton, Nottingham NG14 6NU",
-    dayName: "Wed",
-    day: "01",
-    month: "11",
-    monthName: "November",
-    year: "2024",
+    date: "01/11/2024",
     comp: "Order of Merit",
     courseImage: ramsdale,
     contact: "0115 9655600",
@@ -141,10 +105,7 @@ export const clubs = [
   {
     name: "Oakmere Park Golf Club",
     address: "Oaks Ln, Oxton, Southwell NG25 0RH",
-    dates: [
-      "15/11/2024",
-      "",
-    ],
+    date: "15/11/2024",
     comp: "Order of Merit",
     courseImage: oakmere,
     contact: "0115 9653545",
@@ -154,9 +115,7 @@ export const clubs = [
   {
     name: "Ruddington Golf Club",
     address: "Wilford Rd, Ruddington, Nottingham NG11 6NB",
-    dates: [
-      "28/11/2024",
-    ],
+    date: "28/11/2024",
     comp: "Order of Merit",
     courseImage: ruddington,
     contact: "0115 9214139",
@@ -166,9 +125,7 @@ export const clubs = [
   {
     name: "Bulwell Forest Golf Club",
     address: "Forest Golf Club, Hucknall Rd, Bulwell, Nottingham NG6 9LQ",
-    dates: [
-      "17/01/2024",
-    ],
+    date: "17/01/2024",
     comp: "Order of Merit",
     courseImage: bulwell,
     contact: "0115 9763172",
@@ -176,11 +133,22 @@ export const clubs = [
     logo: bulwellLogo,
   },
 ].sort((a, b) => {
-  const aMilli = new Date(
-    `${a.month}/${a.day}/${a.year}`
-  ).getTime();
-  const bMilli = new Date(
-    `${b.month}/${b.day}/${b.year}`
-  ).getTime();
-  return aMilli - bMilli;
+  const parseDate = (dateString) => {
+    const [day, month, year] = dateString.split('/').map(Number);
+    return new Date(year, month - 1, day).getTime();
+  };
+
+  return parseDate(a.date) - parseDate(b.date);
 });
+
+const today = new Date();
+
+export const futureClubs = clubs.filter(club => {
+  const clubDate = new Date(
+    club.date.split('/').reverse().join('-')
+  );
+  return clubDate >= today;
+});
+
+export const nextFixture = futureClubs[0];
+export const nextFourFixtures = futureClubs.slice(1, 5);
