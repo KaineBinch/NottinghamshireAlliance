bd730b4ce8b0319e8a008aae776d4c1e7c7fcafd
Kaine Binch <kainebinch@live.com>
2024-09-18 19:36:19 +0100

    feat: added text section to pages

--- src/pages/coursesPage.jsx
+++ src/pages/coursesPage.jsx
@@ -9,6 +9,21 @@ const CoursesPage = () => {
     <>
       <PageHeader title="Courses" />
       <hr className="border-black" />
+      <div className="bg-[#D9D9D9] mb-8">
+        <div className="max-w-5xl mx-auto py-5 px-4 sm:px-6 lg:px-8 text-start">
+          <p>
+            Discover the courses that are part of our alliance. These partner
+            courses offer a variety of playing experiences, suited for golfers
+            of all skill levels.
+          </p>
+          <p className="pt-5">
+            Click on any course below to learn more and find out what makes each
+            one a unique destination for your next round.
+          </p>
+        </div>
+        <hr className="border-black" />
+      </div>
+
       <div className="bg-[#D9D9D9]">
         <div className="max-w-5xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
           <div className="">
--- src/pages/oomPage.jsx
+++ src/pages/oomPage.jsx
@@ -10,7 +10,21 @@ const OrderOfMeritPage = () => {
     <>
       <PageHeader title="Order of Merit" />
       <hr className="border-black" />
-      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="bg-[#d9d9d9]">
+        <div className="max-w-5xl mx-auto py-5 px-4 sm:px-6 lg:px-8 text-start">
+          <p>
+            Welcome to the Order of Merit leaderboard. Here, you{"'"}ll find the
+            latest rankings showcasing the top performers.
+          </p>
+          <p className="pt-5">
+            Stay updated with our leaderboard to track progress and plan your
+            next steps.
+          </p>
+        </div>
+        <hr className="border-black" />
+      </div>
+
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 ">
         <ResultsTable columns={columns} rows={rows} />
       </div>
     </>
