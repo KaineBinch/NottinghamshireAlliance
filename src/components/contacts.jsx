4310310e8419b66a5595424e880ad8ee104b6e28
Kaine Binch <kainebinch@live.com>
2025-01-03 17:20:01 +0000

    feat: implemented data from Strapi

--- src/components/contacts.jsx
+++ src/components/contacts.jsx
@@ -3,13 +3,17 @@ const Contacts = ({ name, position, tel, email }) => {
     <>
       <div className="flex flex-col items-start justify-center my-12">
         <h2 className="mb-1">{name}</h2>
-        <div className="my-1 text-lg">{position}</div>
-        <a className="my-1 text-lg" href={`tel:${tel}`}>
-          {tel}
-        </a>
-        <a className="my-1 text-lg" href={`mailto:${email}`}>
-          {email}
-        </a>
+        {position && <a className="my-1 text-lg">{position}</a>}
+        {tel && (
+          <a className="my-1 text-lg" href={`tel:${tel}`}>
+            {tel}
+          </a>
+        )}
+        {email && (
+          <a className="my-1 text-lg" href={`mailto:${email}`}>
+            {email}
+          </a>
+        )}
       </div>
     </>
   );
