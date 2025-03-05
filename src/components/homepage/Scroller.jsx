5d2363dd767088dce770491c2a9b40d27b7b9b8f
Kaine Binch <kainebinch@live.com>
2025-03-05 01:58:51 +0000

    refactor: replace static club logos with dynamic fetching and remove unused constants

--- src/components/homepage/Scroller.jsx
+++ src/components/homepage/Scroller.jsx
@@ -1,7 +1,27 @@
-import { clubLogos } from "../../constants/clubLogos"
+import { queryBuilder } from "../../utils/queryBuilder"
+import { BASE_URL, MODELS, QUERIES } from "../../constants/api"
+import useFetch from "../../utils/hooks/useFetch"
 import ScrollingImages from "./scrollingImages"
 
 const LogoScroller = () => {
+  const query = queryBuilder(MODELS.golfClubs, QUERIES.clubsQuery)
+  const { isLoading, isError, data, error } = useFetch(query)
+
+  if (isLoading) {
+    return <p className="pt-[85px]">Loading...</p>
+  }
+  if (isError) {
+    console.error("Error:", error)
+    return <p className="pt-[85px]">Something went wrong...</p>
+  }
+
+  const clubLogos =
+    data?.data
+      ?.filter(
+        (club) => club.clubLogo && club.clubLogo[0] && club.clubLogo[0].url
+      )
+      .map((club) => `${BASE_URL}${club.clubLogo[0].url}`) || []
+
   return (
     <div className="flex flex-col items-center bg-[#214A27]">
       <hr className="border-black w-full" />
