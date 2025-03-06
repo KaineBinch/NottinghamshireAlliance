68e0f1d8869e7c1c8cd79725d03f03c67b0e6e58
Kaine Binch <kainebinch@live.com>
2025-03-06 17:12:25 +0000

    feat: add PostHog analytics integration and update dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -10,17 +10,25 @@ import {
   faSquareInstagram,
 } from "@fortawesome/free-brands-svg-icons"
 import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { PostHogProvider } from "posthog-js/react"
 
 library.add(fab, faPhone, faEnvelope, faSquareFacebook, faSquareInstagram)
 
+const options = {
+  api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
+}
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </HashRouter>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}>
+      <HashRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </HashRouter>
+    </PostHogProvider>
   </React.StrictMode>
 )
