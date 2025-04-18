3258a598bf80d8cc96aac7ef264cd65cbd23bfa3
KaineBinch <kainebinch@live.com>
2025-04-18 20:42:09 +0100

    feat: enhance environment configuration for PostHog integration and add Node.js support in ESLint

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,41 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: './',
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/ph-proxy': {
-        target: 'https://eu.i.posthog.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/ph-proxy/, '')
-      },
-      '/ph-assets': {
-        target: 'https://eu-assets.i.posthog.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/ph-assets/, '')
-      },
-      '/array': {
-        target: 'https://eu-assets.i.posthog.com',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    base: '/',
+    plugins: [
+      react(),
+      {
+        name: 'html-transform',
+        transformIndexHtml(html) {
+          const posthogKey = env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY;
+          const posthogHost = env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST;
+
+          return html
+            .replace(/%VITE_REACT_APP_PUBLIC_POSTHOG_KEY%/g, posthogKey)
+            .replace(/%VITE_REACT_APP_PUBLIC_POSTHOG_HOST%/g, posthogHost);
+        }
+      }
+    ],
+    server: {
+      proxy: {
+        '/ph-proxy': {
+          target: env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/ph-proxy/, '')
+        },
+        '/ph-assets': {
+          target: env.VITE_REACT_APP_POSTHOG_ASSETS_HOST,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/ph-assets/, '')
+        },
+        '/array': {
+          target: env.VITE_REACT_APP_POSTHOG_ASSETS_HOST,
+          changeOrigin: true
+        }
       }
     }
   }
