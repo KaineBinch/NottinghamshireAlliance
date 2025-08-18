e56557273c9aa35e1aad6e21ab6ffc75cb1f956d
KaineBinch <kainebinch@live.com>
2025-08-18 22:34:01 +0100

    refactor: improve error handling in useFetch hook by ensuring response status is checked before parsing JSON

--- src/utils/hooks/useFetch.js
+++ src/utils/hooks/useFetch.js
@@ -3,8 +3,12 @@ import { useQuery } from "react-query"
 const useFetch = (url) => {
   const fetchData = async () => {
     const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
     return response.json()
   }
+
   return useQuery(url, fetchData)
 }
 
